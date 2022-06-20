4539824a5d4def13d43cabeeda3eefe9f365e6fb
Andrey Yankov <ayankov@ozon.ru>
2022-06-20 16:05:48 +0300

    update test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,41 +1,16 @@
 import math
 from time import sleep
 
+from .pages.main_page import MainPage
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
 
 
-def solve_quiz_and_get_code(browser):
-    alert = browser.switch_to.alert
-    x = alert.text.split(" ")[2]
-    answer = str(math.log(abs((12 * math.sin(float(x))))))
-    alert.send_keys(answer)
-    alert.accept()
-    try:
-        alert = browser.switch_to.alert
-        alert_text = alert.text
-        print(f"Your code: {alert_text}")
-        alert.accept()
-    except NoAlertPresentException:
-        print("No second alert presented")
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
-
-
-def test_add_item_to_card(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn.btn.btn-lg.btn-primary.btn-add-to-basket").click()
-    print(solve_quiz_and_get_code(browser))
-    # sleep(30)
-
-
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
